9ea3e3e25195d04d05399ad6b87db76461ee9b08
Chris Amico <eyeseast@gmail.com>
2010-09-11 10:27:54 -0400

    Added a setup.py script

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+readme = open('README.md', 'rb').read()
+
+setup(
+    name = "simple-sunlight",
+    version = "0.1",
+    description = "A simpler wrapper for Sunlight's Congress API",
+    long_description = readme,
+    author="Chris Amico",
+    author_email="eyeseast@gmail.com",
+    py_modules = ['sunlight'],
+    download_url = "http://github.com/eyeseast/simple-sunlight",
+    url = "http://github.com/eyeseast/simple-sunlight",
+)
